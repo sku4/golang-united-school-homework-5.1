cd3acd1b8181aef892fdfe676c9a56f43a99ebd4
Скубач Сергей <s.skubach@e-comexpert.com>
2022-05-19 22:14:59 +0300

    solution

---
+++ square.go
@@ -0,0 +1,25 @@
+package square
+
+type Point struct {
+	x, y int
+}
+
+type Square struct {
+	start Point
+	a     uint
+}
+
+func (s *Square) End() Point {
+	return Point{
+		s.start.x + int(s.a),
+		s.start.y + int(s.a),
+	}
+}
+
+func (s *Square) Area() uint {
+	return s.a * s.a
+}
+
+func (s *Square) Perimeter() uint {
+	return s.a * 4
+}
